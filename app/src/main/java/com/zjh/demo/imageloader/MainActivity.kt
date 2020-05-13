5a3c32c5d7b502ed6f65e4ed6ed624d773d1275b
superzhangjh <731338858@qq.com>
2020-05-14 00:29:18 +0800

    自定义ViewTarget，实现一步完成图片等比压缩。

--- app/src/main/java/com/zjh/demo/imageloader/MainActivity.kt
+++ app/src/main/java/com/zjh/demo/imageloader/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.zjh.demo.imageloader
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.Transformation
+import com.bumptech.glide.load.engine.Resource
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomViewTarget
+import com.bumptech.glide.request.transition.Transition
 import com.zjh.imageloader.ImageLoader
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
 
-    private val url = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1589257005090&di=dff328646fb37fa17382a084089ec82a&imgtype=0&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D862704645%2C1557247143%26fm%3D214%26gp%3D0.jpg"
+    private val url = "http://attach.bbs.miui.com/forum/201706/16/174318fpxmpt5titpz56qx.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +31,48 @@ class MainActivity : AppCompatActivity() {
         ImageLoader.instance.setOnLowMemory(application)
 
         //...
-        ImageLoader.instance.load(iv, url, maxByteCount = 50000)
+//        ImageLoader.instance.load(iv, url, maxByteCount = 50000)
 
-        iv.setOnClickListener {
-            Toast.makeText(this, "点击", Toast.LENGTH_SHORT).show()
-        }
+//        iv.setOnClickListener {
+//            Toast.makeText(this, "点击", Toast.LENGTH_SHORT).show()
+//        }
+
+        Glide.with(this).load(url)
+            .placeholder(R.drawable.loading)
+            .error(R.drawable.loading)
+            .apply(RequestOptions.bitmapTransform(object : Transformation<Bitmap?> {
+                override fun updateDiskCacheKey(messageDigest: MessageDigest) {
+
+                }
+
+                override fun transform(
+                    context: Context,
+                    resource: Resource<Bitmap?>, //bitmap会生成最小比例
+                    outWidth: Int,  //设置到View时的大小, 以长宽的最大值，去作为最小值获取原有比例的图片大小
+                    outHeight: Int
+                ): Resource<Bitmap?> {
+                    //TODO：这里可以进行图片压缩的操作
+                    //TODO:重写一个CustonViewTarget，然后修改getTargetWidth getTargetHeight
+                    val bitmap = resource.get()
+                    Log.d("测试", "transform outW:$outWidth outH:$outHeight " +
+                            "w:${bitmap.width} h:${bitmap.height} count:${bitmap.byteCount}")
+                    return resource
+                }
+            }))
+            .into(object : CompressViewTarget<View, Drawable>(iv, 0.7) {
+            override fun onLoadFailed(errorDrawable: Drawable?) {
+                Log.d("测试", "CompressViewTarget onLoadFailed")
+            }
+
+            override fun onResourceCleared(placeholder: Drawable?) {
+                Log.d("测试", "CompressViewTarget onResourceCleared")
+
+            }
+
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                val bitmap = (resource as BitmapDrawable).bitmap
+                Log.d("测试", "CompressViewTarget ${bitmap.width} ${bitmap.height} count:${bitmap.byteCount}")
+            }
+        })
     }
 }
